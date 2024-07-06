5d872d6d001f09fd03c2f603c838ba93446565d0
pucetec <jatipanb@puce.edu.ec>
2024-07-06 10:10:41 -0500

    Terminado el laboratorio

--- lab8/settings.py
+++ lab8/settings.py
@@ -78,7 +78,15 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    },
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': 'pokedex',
+#        'HOST': 'localhost',
+#        'PORT': '5432',
+#        'USER': 'postgres',
+#        'PASSWORD': 'postgres'
+#   }
 }
 
 
